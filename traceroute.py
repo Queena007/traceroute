67c10526a7f0bb6baabb93064b9c7b3772395349
Queena007 <124480642+Queena007@users.noreply.github.com>
2023-04-08 11:25:12 -0400

    Update traceroute.py

--- traceroute.py
+++ traceroute.py
@@ -13,62 +13,36 @@ MAX_HOPS = 60
 TIMEOUT = 2.0
 TRIES = 1
 
-# ... (checksum and build_packet functions remain the same)
+def checksum(source_string):
+    sum = 0
+    countTo = (len(source_string) // 2) * 2
+    count = 0
+    while count < countTo:
+        thisVal = source_string[count + 1] * 256 + source_string[count]
+        sum = sum + thisVal
+        sum = sum & 0xffffffff
+        count = count + 2
+    if countTo < len(source_string):
+        sum = sum + source_string[len(source_string) - 1]
+        sum = sum & 0xffffffff
+    sum = (sum >> 16) + (sum & 0xffff)
+    sum = sum + (sum >> 16)
+    answer = ~sum
+    answer = answer & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+    return answer
 
-def get_route(hostname):
-    timeLeft = TIMEOUT
-    df = pd.DataFrame(columns=['Hop Count', 'Try', 'IP', 'Hostname', 'Response Code'])
-    destAddr = gethostbyname(hostname)
-   
-    for ttl in range(1, MAX_HOPS):
-        for tries in range(TRIES):
- 
-            icmp = getprotobyname("icmp")
-            mySocket = socket(AF_INET, SOCK_RAW, icmp)
+def build_packet():
+    myChecksum = 0
+    myID = os.getpid() & 0xFFFF
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, myChecksum, myID, 1)
+    data = struct.pack("d", time.time())
+    myChecksum = checksum(header + data)
+    header = struct.pack("bbHHh", ICMP_ECHO_REQUEST, 0, socket.htons(myChecksum), myID, 1)
+    packet = header + data
+    return packet
 
-            mySocket.setsockopt(IPPROTO_IP, IP_TTL, struct.pack('I', ttl))
-            mySocket.settimeout(TIMEOUT)
-            try:
-                d = build_packet()
-                mySocket.sendto(d, (hostname, 0))
-                t = time.time()
-                startedSelect = time.time()
-                whatReady = select.select([mySocket], [], [], timeLeft)
-                howLongInSelect = (time.time() - startedSelect)
-                if not whatReady[0]:  # Timeout
-                    print("*    *    * Request timed out.")
-                    df = df.append({'Hop Count': ttl, 'Try': tries, 'IP': "", 'Hostname': "", 'Response Code': "Request timed out"}, ignore_index=True)
-                recvPacket, addr = mySocket.recvfrom(1024)
-                timeReceived = time.time()
-                timeLeft = timeLeft - howLongInSelect
-
-                if timeLeft <= 0:
-                    df = df.append({'Hop Count': ttl, 'Try': tries, 'IP': addr[0], 'Hostname': "", 'Response Code': "Request timed out"}, ignore_index=True)
-            except Exception as e:
-                print(e)  # uncomment to view exceptions
-                continue
-
-            else:
-                icmpHeader = recvPacket[20:28]
-                types, code, checksum, packetID, sequence = struct.unpack("bbHHh", icmpHeader)
-
-                try:
-                    router_hostname = gethostbyaddr(addr[0])[0]
-                except herror:
-                    router_hostname = "hostname not returnable"
-
-                if types == 11:
-                    df = df.append({'Hop Count': ttl, 'Try': tries, 'IP': addr[0], 'Hostname': router_hostname, 'Response Code': "TTL Exceeded"}, ignore_index=True)
-                elif types == 3:
-                    df = df.append({'Hop Count': ttl, 'Try': tries, 'IP': addr[0], 'Hostname': router_hostname, 'Response Code': "Destination Unreachable"}, ignore_index=True)
-                elif types == 0:
-                    df = df.append({'Hop Count': ttl, 'Try': tries, 'IP': addr[0], 'Hostname': router_hostname, 'Response Code': "Echo Reply"}, ignore_index=True)
-                    return df
-                else:
-                    df = df.append({'Hop Count': ttl, 'Try': tries, 'IP': addr[0], 'Hostname': router_hostname, 'Response Code': "Error"}, ignore_index=True)
-                break
-    print(df)
-    return df
+# ... (get_route function remains the same)
 
 if __name__ == '__main__':
     get_route("google.co.il")
